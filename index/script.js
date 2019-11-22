a32658a5064c1e60434491b33b4b3264d85ae0aa
Doublefs <amfinelli2001@gmail.com>
2019-11-22 10:35:33 -0500

    Update script.js
    JS

--- index/script.js
+++ index/script.js
@@ -1,1 +1,13 @@
+{
+    //Create array for all of the pads
+    var padArray = document.getElementsByClassName("pad");
 
+    padArray[0].onclick = function(){onclick(0)};
+    padArray[1].onclick = function(){onclick(1)};
+
+    function onClick(num) {
+        console.log(num);
+    }
+
+    
+}
