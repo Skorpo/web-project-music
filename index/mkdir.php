618dcf07ed754a9a77f72982ef2db13d27aee8ca
poulor <rjpoulose@gmail.com>
2019-11-22 20:39:07 -0500

    mkdir.php now creates most recently created user's files

--- index/mkdir.php
+++ index/mkdir.php
@@ -17,47 +17,47 @@
               echo "Failed to connect to MySQL: (" . $db->connect_errno . ") " . $db->connect_error;
          }
 
-        echo "current dir: " . getcwd() . "<br>";
-
         $structure = getcwd() . "/user_files/";
-        echo "Changing permissions for: " . $structure . "<br>";
 
-        chmod($structure, 0770);
-
-        // echo "PERMISSIONS: " . substr(sprintf('%o', fileperms($structure)), -4);
-
+        // Getting the max id (most recently created user)
         $sql = "SELECT MAX(id) FROM users";
         $result = $db->query($sql);
 
         if (mysqli_num_rows($result) == 1) {
-            // output data of each row
             $row = mysqli_fetch_assoc($result);
 
             $structure .= $row["MAX(id)"] . "/";
-            echo "structure: " . $structure . "<br>";
 
+            // code to remove a user and their files
+            /* rmdir($structure . "recordings/");
+             rmdir($structure . "sounds/");
+             rmdir($structure);*/
 
-            if (!mkdir($structure, 0770, true)) {
-                die('Failed to create user id folder!');
+            if (!is_dir($structure)){
+                if (!mkdir($structure, 0777, true)) {
+                    die('Failed to create user id folder!');
+                }
+                else{
+                    echo "Created user id folder!" . "<br>";;
+                }
+
+                if (!mkdir($structure . "recordings/", 0777, true)) {
+                    die('Failed to create recordings folder!');
+                }
+                else{
+                    echo "Created recordings folder!" . "<br>";;
+                }
+
+                if (!mkdir($structure . "sounds/", 0777, true)) {
+                    die('Failed to create sounds folder!');
+                }
+                else{
+                    echo "Created sounds folder!" . "<br>";;
+                }
             }
             else{
-                echo "Created user id folder!";
+                echo "Directory with id " . $row["MAX(id)"] . " already exists!" . "<br>";
             }
-
-            if (!mkdir($structure . "recordings/")) {
-                die('Failed to create recordings folder!');
-            }
-            else{
-                echo "Created recordings folder!";
-            }
-
-            if (!mkdir($structure . "sounds/")) {
-                die('Failed to create sounds folder!');
-            }
-            else{
-                echo "Created sounds folder!";
-            }
-
 
         } else {
             echo " 0 or > 1 results -> results: " . mysqli_num_rows($result);
